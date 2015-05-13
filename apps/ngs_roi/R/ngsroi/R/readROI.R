301725a3a0f201a2fc0f55f390c506afe659a684
baj12 <bernd.jagla@pasteur.fr>
2015-05-13 11:30:30 +0200

    ???

--- apps/ngs_roi/R/ngsroi/R/readROI.R
+++ apps/ngs_roi/R/ngsroi/R/readROI.R
@@ -1,62 +1,57 @@
-readROI <-
-function(file.name){
-   # return element from string array
+readROI <-function(fpname){
    fi <- function(x,i){
       x[i]
    }
-   # return element from string array as number
    fni <- function(x,i){
       as.numeric(x[[i]])
    }
-   # return vector of counts for last column
    getVec <- function(x){
       mylen=as.numeric(x[5])
       veclen=length(x)
-      as.numeric(unlist(strsplit(x[veclen],",")))
+      t1=veclen - mylen + 1
+      mylist = unlist(x)
+      as.list(as.numeric(mylist[c(t1:veclen)]))
    }
-   #get non-fixed values (i.e. all columns between the cg_count and the count vector
    getVals <- function(y, x, columnNames){
-      #mylen=as.numeric(x[[1]][5])
-      veclen=length(columnNames)
-      t1=veclen - 1
+      mylen=as.numeric(x[[1]][5])
+      veclen=length(x[[1]])
+      t1=veclen - mylen
       for ( colN in c(7:t1)) {
    	y[,columnNames[colN]]=unlist(lapply(x,fni,colN))
       }
       return(y)
    }
 
-   con=gzfile(file.name)
-   rLines = readLines(con)
-   close(con)
+   
+   rLines = readLines(gzfile(fpname))
    values = rLines[substr(rLines,1,1)!="#"]
    values = strsplit(values,"\t")
-   if (length(rLines[substr(rLines,1,2)=="##"])==0){
-      columnNames=unlist(list("##ref", 
-                              "begin_pos",
-			      "end_pos",
-			      "region_name",
-			      "length", 
-			      "strand",
-			      "max_count",
-			      "gc_content",
-			      "counts"))
+   if (length(rLines[substr(rLines,1,1)=="##"])==0){
+      columnNames=unlist(list("# 1.  column: reference name",
+         "# 2.  column: starting position on reference genome",
+         "# 3.  column: end position on reference genome" ,
+         "# 4.  column: strand +/-(. if empty)" ,
+         "# 5.  column: length of region" ,
+         "# 6.  column: annotation" ,
+         "# 7.  column: max_Count" ,
+         "# 8+. column: individual counts" ))
    }else{
-      columnNames = unlist(strsplit(rLines[substr(rLines,1,2)=="##"],"\t"))
+      columnNames = unlist(rLines[substr(rLines,1,2)=="##"])
    }
 
 
 
-   df=data.frame(ref=unlist(lapply(values,fi,1)),
-                 begin_pos=as.integer(unlist(lapply(values,fni,2))),
-                 end_pos=as.integer(unlist(lapply(values,fni,3))),
-                 region_name=unlist(lapply(values,fi,4)),
-                 length=as.integer(unlist(lapply(values,fni,5))),
-                 strand=unlist(lapply(values,fi,6))
-		 )
-
+   df=data.frame(id=unlist(lapply(values,fi,1)),
+                 start=unlist(lapply(values,fni,2)),
+                 end=unlist(lapply(values,fni,3)),
+                 strand=unlist(lapply(values,fi,4)),
+                 length=unlist(lapply(values,fni,5)),
+                 annot=unlist(lapply(values,fi,6)))
    df=getVals(df, values, columnNames)
-   df$counts = lapply(values,getVec)
-   df$counts=lapply(df$counts,unlist)
+   df$vec = lapply(values,getVec)
+   df$vec=lapply(df$vec,unlist)
    roiNames=names(df)
+   names(df) <- sub("# .*. column: ","",roiNames)
    return (df)
 }
+
